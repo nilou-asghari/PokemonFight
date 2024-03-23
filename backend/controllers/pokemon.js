889cbd073d57e619a9eb41f2bacc4db5e389198e
Maiwand Aryan <maiwandaryan@gmail.com>
2024-03-23 14:28:52 +0100

    API are fixed with image

--- backend/controllers/pokemon.js
+++ backend/controllers/pokemon.js
@@ -2,20 +2,28 @@ const pokeData = require("../schemas/Pokemon");
 
 const getAllPokemons = () => {
   const res = pokeData.map((item) => {
-    return { ...item, name: item.name.english };
+    let zeros = "";
+    if (item.id < 10) {
+      zeros = "00";
+    } else if (item.id > 10 && item.id < 100) {
+      zeros = "0";
+    }
+
+    let image_url = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${zeros}${item.id}.png`;
+    return { ...item, image: image_url };
   });
   return res;
 };
 
 const getById = (id) => {
-  const filtered = pokeData.filter((item) => item.id == id);
+  const filtered = getAllPokemons().filter((item) => item.id == id);
   return filtered[0];
 };
 
 const getInfoById = (id, info) => {
-  const item = pokeData.filter((item) => item.id == id)[0];
-  const maped = { ...item, name: item.name.english };
-  return maped[info];
+  const item = getAllPokemons().filter((item) => item.id == id)[0];
+  // const maped = { ...item, name: item.name.english };
+  return item[info];
 };
 
 module.exports = { getAllPokemons, getById, getInfoById };
