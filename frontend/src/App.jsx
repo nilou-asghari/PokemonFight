6a8f9c2ee5460876d5f04dcb17bfb1122b172cdb
RobinBigus <robin_bigus@web.de>
2024-03-23 14:19:17 +0100

    started pokemon fight

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,34 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import AllPokemons from "./components/AllPokemons";
+import PokemonFight from "./components/PokemonFight";
 import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const res = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=30"
+        );
+        setPokemons(res.data.results);
+        console.log(res.data.results);
+      } catch (error) {
+        console.error("Error fetching Pokémon data:", error);
+      }
+    };
+    fetchPokemons();
+  }, []);
+
   return (
     <>
       <Navbar />
       <Routes>
         <Route path="/" element={<AllPokemons />} />
+        <Route path="/pokemon/fight" element={<PokemonFight />} />
       </Routes>
     </>
   );
---
+++ frontend/src/components/PokemonFight.jsx
@@ -0,0 +1,16 @@
+import styles from "../components/PokemonFight.module.css";
+
+const PokemonFight = () => {
+  return (
+    <div className={styles.background}>
+      <div>
+        <div></div>
+        <h1></h1>
+        <div></div>
+        <button></button>
+      </div>
+    </div>
+  );
+};
+
+export default PokemonFight;
